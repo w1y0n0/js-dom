79aab05b0e59d20c02b0d5644221fe3022ddb314
w1y0n0 <wiyono30@gmail.com>
2023-07-26 09:34:03 +0700

    Akses Element by querySelector

--- main.js
+++ main.js
@@ -30,3 +30,12 @@ console.log(id_desc.textContent);
 const class_tgl = document.getElementsByClassName('tanggal');
 console.log(class_tgl);
 console.log(class_tgl[0].textContent);
+
+/**
+ * Akses Element by query selector
+ */
+// const data_desc = document.querySelector('p'); // by tag
+const data_desc = document.querySelector('#deskripsi'); // by id
+console.log(data_desc.textContent);
+const data_tgl = document.querySelector('.tanggal'); // by className
+console.log(data_tgl.textContent);
